b8c87ad1085f47d24f7e40e24f47c5aefc6490d3
msbluesnow <msbluesnow69@gmail.com>
2026-08-15 19:24:38 +0300

    chore: bumped version 0.7.1 alpha (versionCode 10)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "dev.msbs.cyclauncher"
         minSdk = 24
         targetSdk = 36
-        versionCode = 9
-        versionName = "0.7.0 Alpha"
+        versionCode = 10
+        versionName = "v0.7.1-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
