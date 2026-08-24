90fa516a5b20cd9f29a0b4c88594ec8b9fa1bb7f
msbluesnow <msbluesnow69@gmail.com>
2026-08-24 17:10:45 +0300

    chore(release): bump version to v0.8.1-alpha (13) and add changelog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "dev.msbs.cyclauncher"
         minSdk = 24
         targetSdk = 36
-        versionCode = 12
-        versionName = "v0.8.0-alpha"
+        versionCode = 13
+        versionName = "v0.8.1-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
