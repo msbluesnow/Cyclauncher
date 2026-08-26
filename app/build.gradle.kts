6388891df6691464aeeb428b0375a5df4aa0e118
msbluesnow <msbluesnow69@gmail.com>
2026-08-27 00:26:24 +0300

    bump: release v0.8.2-alpha (versionCode 14)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "dev.msbs.cyclauncher"
         minSdk = 24
         targetSdk = 36
-        versionCode = 13
-        versionName = "v0.8.1-alpha"
+        versionCode = 14
+        versionName = "v0.8.2-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
